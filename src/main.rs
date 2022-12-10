a927bee116ff4c518da14b4a51e89d6d26308fce
Alberto Contreras <alberto.contreras@canonical.com>
2022-12-10 23:56:05 +0100

    play around with validator and server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod validator;
+
+use jsonschema::output::BasicOutput;
+use validator::Validator;
+
+fn validate_test(validator: &Validator, instance: &str) {
+    let instance = serde_json::from_str(instance).unwrap();
+    match validator.validate(&instance) {
+        BasicOutput::Valid(annotations) => {
+            for annotation in annotations {
+                println!(
+                    "Value: {} at path {}",
+                    annotation.value(),
+                    annotation.instance_location()
+                )
+            }
+        }
+        BasicOutput::Invalid(errors) => {
+            dbg!(&errors);
+            for error in errors {
+                println!(
+                    "Error: {} at path {}",
+                    error.error_description(),
+                    error.instance_location()
+                )
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let validator = Validator::new();
+    let instance = "";
+    validate_test(&validator, instance);
 }
