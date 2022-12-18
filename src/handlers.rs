546272186274404a99b9fe148c5ed8003f7cd671
Alberto Contreras <alberto.contreras@canonical.com>
2022-12-18 18:58:59 +0100

    validator: remove json input format

--- src/handlers.rs
+++ src/handlers.rs
@@ -5,16 +5,12 @@ use axum::{
 };
 use serde_json::Value;
 
-use crate::validator::{CloudConfig, Format, Validator};
+use crate::validator::{CloudConfig, Validator};
 
 pub async fn validate(Json(payload): Json<CloudConfig>) -> (StatusCode, Json<Value>) {
     // TODO: share the validator at higher level point
     let validator = Validator::new();
-
-    let payload: Value = match payload.format() {
-        Format::Yaml => serde_yaml::from_str(payload.payload()).unwrap(),
-        Format::Json => serde_json::from_str(payload.payload()).unwrap(),
-    };
+    let payload = serde_yaml::from_str(payload.payload()).unwrap();
 
     let resp = validator.validate(&payload);
     let resp = serde_json::to_value(resp).unwrap();
